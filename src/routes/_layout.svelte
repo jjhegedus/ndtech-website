0a2bf882fd8019f57c637bc31669818ba23c1497
Kim Hegedus <kim@22ndtech.com>
2021-02-08 09:04:30 -0700

    Google AdSense script added to Layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,6 +6,9 @@
 
 <LoadingBar/>
 <Nav/>
+<head>
+	<script data-ad-client="ca-pub-6882490618446355" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
 
 <main>
 	<slot></slot>
